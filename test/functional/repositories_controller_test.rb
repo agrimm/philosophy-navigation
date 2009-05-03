1550ceff609cf63f3d3ad0a98f11ee5969291442
Andrew Grimm <andrew.j.grimm@gmail.com>
2009-05-03 18:26:34 +1000

    Got rid of modification of repositories.

--- test/functional/repositories_controller_test.rb
+++ test/functional/repositories_controller_test.rb
@@ -7,39 +7,9 @@ class RepositoriesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:repositories)
   end
 
-#  test "should get new" do
-#    get :new
-#    assert_response :success
-#  end
-
-#  test "should create repository" do
-#    assert_difference('Repository.count') do
-#      post :create, :repository => { }
-#    end
-
-#    assert_redirected_to repository_path(assigns(:repository))
-#  end
-
   test "should show repository" do
     get :show, :id => repositories(:one).to_param
     assert_response :success
   end
 
-#  test "should get edit" do
-#    get :edit, :id => repositories(:one).to_param
-#    assert_response :success
-#  end
-
-#  test "should update repository" do
-#    put :update, :id => repositories(:one).to_param, :repository => { }
-#    assert_redirected_to repository_path(assigns(:repository))
-#  end
-
-#  test "should destroy repository" do
-#    assert_difference('Repository.count', -1) do
-#      delete :destroy, :id => repositories(:one).to_param
-#    end
-
-#    assert_redirected_to repositories_path
-#  end
 end
